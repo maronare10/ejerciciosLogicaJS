f149f8cf5a1628c7db27b2cdc04ae3eca9c3fdc0
Mariana Navarro <maronare10@gmail.com>
2023-03-11 17:18:59 -0500

    ejercicios de javascript - lógica

---
+++ app.js
@@ -0,0 +1,118 @@
+// En los siguientes ejercicios deberás reemplazar el valor null
+// por el correspondiente.
+
+// Crea una variable de tipo string.
+const nuevoString = null;
+
+// Crea una variable de tipo number.
+const nuevoNumero = null;
+
+// Crea una variable de tipo boolean.
+const nuevoBoolean = null;
+
+// Resuelve el siguiente problema matemático.
+const nuevaResta = 10 - null === 3;
+
+// Resuelve el siguiente problema matemático.
+const nuevaMultiplicacion = 10 * null === 40;
+
+// Resuelve el siguiente problema matemático.
+const nuevoModulo = 21 % 5 === null;
+
+function devolverString(string) {
+  // Debe retornar un string.
+  // Tu código:
+}
+
+// ⛔️ "X" e "Y" son números.
+
+function suma(x, y) {
+  // Retorna el resultado de su suma.
+  // Tu código:
+}
+
+function resta(x, y) {
+  // Retorna el resultado de la resta.
+  // Tu código:
+}
+
+function divide(x, y) {
+  // Retorna el resultado de su división.
+  // Tu código:
+}
+
+function multiplica(x, y) {
+  // Retorna el resultado de su multiplicación.
+  // Tu código:
+}
+
+function obtenerResto(x, y) {
+  // Obten el resto de la división de "x" entre "y".
+  // Tu código:
+}
+
+function sonIguales(x, y) {
+  // Retorna true si "x" e "y" son iguales.
+  // De lo contrario, retorna false.
+  // Tu código:
+}
+
+function tienenMismaLongitud(str1, str2) {
+  // Retorna true si los dos strings tienen la misma longitud.
+  // De lo contrario, retorna false.
+  // Tu código:
+}
+
+function menosQueNoventa(num) {
+  // Retorna true si el argumento "num" es menor que noventa.
+  // De lo contrario, retorna false.
+  // Tu código:
+}
+
+function mayorQueCincuenta(num) {
+  // Retorna true si el argumento "num" es mayor que cincuenta.
+  // De lo contrario, retorna false.
+  // Tu código:
+}
+
+function esPar(num) {
+  // Retorna true si "num" es par.
+  // De lo contrario, retorna false.
+  // Tu código:
+}
+
+function esImpar(num) {
+  // Retorna true si "num" es impar.
+  // De lo contrario, retorna false.
+  // Tu código:
+}
+
+function elevarAlCuadrado(num) {
+  // Retorna el valor de "num" elevado al cuadrado.
+  // Tu código:
+}
+
+function elevarAlCubo(num) {
+  // Retorna el valor de "num" elevado al cubo.
+  // Tu código:
+}
+
+function elevar(num, exponent) {
+  // Retorna el valor de "num" elevado al exponente "exponent".
+  // Tu código:
+}
+
+function redondearNumero(num) {
+  // Redondea "num" al entero más próximo y retórnalo.
+  // Tu código:
+}
+
+function redondearHaciaArriba(num) {
+  // Redondea "num" hacia arriba y retórnalo.
+  // Tu código:
+}
+
+function numeroRandom() {
+  // Genera un número al azar entre 0 y 1 y retórnalo.
+  // Tu código:
+}
